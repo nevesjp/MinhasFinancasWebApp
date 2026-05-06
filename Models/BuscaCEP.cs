df4d34fadb145e6cde6966ede61f20be0b219269
nevesjp <jamersonp53@gmail.com>
2026-05-05 22:54:09 -0300

    adicionando id e dataConsulta; corrigindo nome da string district

--- Models/BuscaCEP.cs
+++ Models/BuscaCEP.cs
@@ -4,15 +4,16 @@ namespace MinhasFinancasWebApp.Models
 {
     public class RegistroCEP
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string cepConsultaText { get; set; }
-
         public string code { get; set; }
         public string state { get; set; }
         public string city { get; set; }
-        public string discrict { get; set; }
+        public string district { get; set; }
         public string address { get; set; }
         public int status { get; set; }
         public bool ok { get; set; }
         public string statusText { get; set; }
+        public DateTime dataConsulta { get; set; } = DateTime.Now;
     }
 }
